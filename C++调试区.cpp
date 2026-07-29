91ff2142f6556c6a72cf1b03e7f2a10d0808eb8c
lxj917917 <2273936277@qq.com>
2026-07-29 11:08:49 +0800

    vault backup: 2026-07-29 11:08:49

--- C++调试区.cpp
+++ C++调试区.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    int score = 0;
+    /*int score = 0;
     cout << "请输入您的成绩：" << endl;
     cin >> score;
     if (score >= 600)
@@ -14,8 +15,36 @@ int main()
     else 
     {
         cout << "很遗憾，您没有考上一本大学" << endl;
-    }
+    }*/
+
+
+    //猜数字
+    //调用C++的库 rand()
+
+    int random = rand() % 100 + 1;
+    int guess_number = 0;
     
+    while (1)
+    {
+        cout << "请输入您猜的数字：" << endl;
+    
+    cin >> guess_number;
+    if (guess_number > random)
+    {
+        cout << "您猜的数字大了" << endl;
+    }
+    else if (guess_number < random)
+    {
+        cout << "您猜的数字小了" << endl;
+    }
+    else
+    {
+        cout << "恭喜您，猜对了" << endl;
+        break;
+
+    }
+    }
+
     system("pause");
     return 0;
 }
