cb8ff5b50638380d44b9b9ae0ab7d4d782984fe6
lxj917917 <2273936277@qq.com>
2026-07-29 10:36:18 +0800

    vault backup: 2026-07-29 10:36:18

--- C++调试区.cpp
+++ C++调试区.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    
+    int score = 0;
+    cout << "请输入您的成绩：" << endl;
+    cin >> score;
+    if (score >= 600)
+    {
+        cout << "恭喜您，考上了一本大学" << endl;
+    }
     
     system("pause");
     return 0;
