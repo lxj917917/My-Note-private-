53b9a5226a644d4bac16708409c5dd0b209ee72f
lxj917917 <2273936277@qq.com>
2026-07-30 15:27:42 +0800

    vault backup: 2026-07-30 15:27:42

--- C++调试区.cpp
+++ C++调试区.cpp
@@ -3,73 +3,45 @@
 #include <ctime>
 
 using namespace std;
-int add(int a,int b);
+void bubblesort(int *p,int n);
+void print(int *p,int n);
 
 int main()
 {
-    /*int score = 0;
-    cout << "请输入您的成绩：" << endl;
-    cin >> score;
-    if (score >= 600)
-    {
-        cout << "恭喜您，考上了一本大学" << endl;
-    }
-    else 
-    {
-        cout << "很遗憾，您没有考上一本大学" << endl;
-    }*/
+    //指针、函数、数组实现冒泡排序
+    int arr[10] = {4,3,6,9,1,2,10,5,8,7};
+    int n = 10;
 
-
-    //猜数字
-    //调用C++的库 rand()
-    /*srand((unsigned int)time(NULL));     // 初始化随机数种子
-    int random = rand() % 100 + 1;
-    int guess_number = 0;
-    
-    while (1)
-    {
-        cout << "请输入您猜的数字：" << endl;
-    
-    cin >> guess_number;
-    if (guess_number > random)
-    {
-        cout << "您猜的数字大了" << endl;
-    }
-    else if (guess_number < random)
-    {
-        cout << "您猜的数字小了" << endl;
-    }
-    else
-    {
-        cout << "恭喜您，猜对了" << endl;
-        break;
-
-    }
-    }*/
-
-    //敲桌子
-    /*for (int i = 0;i<100;i++)
-    {
-        if (i % 7 == 0 || i / 10 == 7 || i % 10 == 7)
-        {
-            cout << "敲桌子" << endl;
-        }
-        else
-        {
-            cout << i << endl;
-        }
-    }*/
-
-    //函数实现相加
-    int sum = 0;
-    sum = add(10,20);
-    cout << "sum = " << sum << endl;
+    int *p = arr;
+    print(p, n);
+    bubblesort(p, n);
+    print(p, n);
 
     system("pause");
     return 0;
 }
 
-int add(int a,int b)
+void bubblesort(int *p,int n)
 {
-    return a+b;
+    for (int i = 0; i < n -1;i++)
+    {
+        for (int j = 0;j<n-1-i;j++)
+        {
+            if (*(p+j) > *(p+j+1))
+            {
+                int temp = *(p+j);
+                *(p+j) = *(p+j+1);
+                *(p+j+1) = temp;
+            }
+        }
+    }
+}
+
+void print(int *p,int n)
+{
+    for (int i = 0; i < n;i++)
+    {
+        cout << *(p+i) << " ";
+    }
+    cout << endl;
 }
