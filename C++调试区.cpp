0b20884fd0bae189ef1c03b11d6340766424252d
lxj917917 <2273936277@qq.com>
2026-08-07 11:34:26 +0800

    vault backup: 2026-08-07 11:34:26

--- C++调试区.cpp
+++ C++调试区.cpp
@@ -3,45 +3,29 @@
 #include <ctime>
 
 using namespace std;
-void bubblesort(int *p,int n);
-void print(int *p,int n);
+const double PI = 3.1415926;
+
+class circal
+{
+    public:
+
+    int m_r;
+
+    double calculateZC()
+    {
+            return 2 * PI * m_r;
+    }
+};
+
 
 int main()
 {
-    //指针、函数、数组实现冒泡排序
-    int arr[10] = {4,3,6,9,1,2,10,5,8,7};
-    int n = 10;
-
-    int *p = arr;
-    print(p, n);
-    bubblesort(p, n);
-    print(p, n);
+    circal c1;
+    cout << "请输入圆的半径：" << endl;
+    cin >> c1.m_r;
+    cout << "圆的周长为：" << c1.calculateZC() << endl;
 
     system("pause");
     return 0;
 }
 
-void bubblesort(int *p,int n)
-{
-    for (int i = 0; i < n -1;i++)
-    {
-        for (int j = 0;j<n-1-i;j++)
-        {
-            if (*(p+j) > *(p+j+1))
-            {
-                int temp = *(p+j);
-                *(p+j) = *(p+j+1);
-                *(p+j+1) = temp;
-            }
-        }
-    }
-}
-
-void print(int *p,int n)
-{
-    for (int i = 0; i < n;i++)
-    {
-        cout << *(p+i) << " ";
-    }
-    cout << endl;
-}
